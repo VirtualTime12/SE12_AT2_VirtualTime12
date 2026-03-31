b5638f99750780bae833255bd787f86e1b423019
VirtualTime12 <michael.ryman08@gmail.com>
2026-03-31 07:17:24 +0000

    Completed Signup/Login and Favourites functions

--- database_manager.py
+++ database_manager.py
@@ -1,4 +1,5 @@
 import sqlite3 as sql
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 def listVtuberbygeneration(generation):
@@ -42,14 +43,108 @@ def get_vtuber(id):
     vtuber = cur.execute("SELECT * FROM vtubers WHERE id = ?", (id,)).fetchone()
 
     if vtuber is None:
+        con.close()
         return "VTuber not found", 404
     else:
+        con.close()
         return vtuber
 
 
-def insertContact(email, name):
+def insertUser(email, password):
+    hashed_pw = generate_password_hash(password)
+
     con = sql.connect("database/data_source.db")
     cur = con.cursor()
-    cur.execute("INSERT INTO contact_list (email,name) VALUES (?,?)", (email, name))
+    cur.execute(
+        "INSERT INTO users (email,password) VALUES (?,?)",
+        (email, hashed_pw),
+    )
     con.commit()
     con.close()
+
+
+def checkUser(email, password):
+    con = sql.connect("database/data_source.db")
+    cur = con.cursor()
+
+    cur.execute("SELECT password FROM users WHERE email = ?", (email,))
+    user = cur.fetchone()
+
+    if user is None:
+        con.close()
+        return False
+
+    stored_password = user[0]
+
+    if check_password_hash(stored_password, password):
+        con.close()
+        return True
+    else:
+        con.close()
+        return False
+
+
+def get_user(email):
+    con = sql.connect("database/data_source.db")
+    cur = con.cursor()
+
+    user = cur.execute("SELECT * FROM users WHERE email = ?", (email,)).fetchone()
+    con.close()
+    return user
+
+
+def add_favourite(user_id, vtuber_id):
+    con = sql.connect("database/data_source.db")
+    cur = con.cursor()
+    try:
+        cur.execute(
+            "INSERT INTO favourites (user_id, vtuber_id) VALUES (?, ?)",
+            (user_id, vtuber_id),
+        )
+        con.commit()
+    except:
+        pass
+    con.close()
+
+
+def remove_favourite(user_id, vtuber_id):
+    con = sql.connect("database/data_source.db")
+    cur = con.cursor()
+
+    cur.execute(
+        "DELETE FROM favourites WHERE user_id=? AND vtuber_id=?", (user_id, vtuber_id)
+    )
+    con.commit()
+    con.close()
+
+
+def is_favourite(user_id, vtuber_id):
+    con = sql.connect("database/data_source.db")
+    cur = con.cursor()
+
+    result = cur.execute(
+        "SELECT 1 FROM favourites WHERE user_id=? AND vtuber_id=?", (user_id, vtuber_id)
+    ).fetchone()
+
+    con.close()
+
+    return result is not None
+
+
+def get_favourites(user_id):
+    con = sql.connect("database/data_source.db")
+    cur = con.cursor()
+
+    results = cur.execute(
+        """
+        SELECT vtubers.*
+        FROM vtubers
+        JOIN favourites ON vtubers.id = favourites.vtuber_id
+        WHERE favourites.user_id = ?
+    """,
+        (user_id,),
+    ).fetchall()
+
+    con.close()
+
+    return results
--- main.py
+++ main.py
@@ -1,25 +1,39 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, redirect, request, session
 import database_manager as dbHandler
 
 app = Flask(__name__)
+app.secret_key = "TESTING123"
+
+
+def logged_in():
+    if "user_id" in session:
+        return True
+    else:
+        return False
 
 
 @app.route("/home.html", methods=["GET"])
 @app.route("/", methods=["POST", "GET"])
 def home():
-    return render_template("/home.html")
+    return render_template("/home.html", logged_in=logged_in())
 
 
 @app.route("/vtuber/<int:id>")
 def vtuber_page(id):
-
     vtuber = dbHandler.get_vtuber(id)
-    return render_template("vtuber.html", vtuber=vtuber)
+    is_favourited = False
+
+    if "user_id" in session:
+        is_favourited = dbHandler.is_favourite(session["user_id"], id)
+
+    return render_template(
+        "vtuber.html", vtuber=vtuber, logged_in=logged_in(), is_favourited=is_favourited
+    )
 
 
 @app.route("/about.html", methods=["GET"])
 def about():
-    return render_template("/about.html")
+    return render_template("/about.html", logged_in=logged_in())
 
 
 @app.route("/index.html", methods=["GET"])
@@ -40,18 +54,78 @@ def index():
 
     # Sort graduated Vtubers after active Vtubers
     data.sort(key=lambda row: (row[11]))
-    return render_template("/index.html", content=data)
+    return render_template("/index.html", content=data, logged_in=logged_in())
 
 
-@app.route("/add.html", methods=["POST", "GET"])
-def add():
+@app.route("/signup.html", methods=["POST", "GET"])
+def signup():
     if request.method == "POST":
         email = request.form["email"]
-        name = request.form["name"]
-        dbHandler.insertContact(email, name)
-        return render_template("/add.html", is_done=True)
+        password = request.form["password"]
+        dbHandler.insertUser(email, password)
+        return render_template(
+            "/login.html",
+            message="Signup Successful, Now please Login using those same credentials",
+        )
     else:
-        return render_template("/add.html")
+        return render_template("/signup.html", logged_in=logged_in())
+
+
+@app.route("/login.html", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        user = dbHandler.get_user(email)
+
+        if dbHandler.checkUser(email, password):
+            session["user_id"] = user[0]
+            return render_template("/home.html", logged_in=logged_in())
+        else:
+            return render_template(
+                "/login.html", message="Invalid Login", logged_in=logged_in()
+            )
+    else:
+        return render_template("/login.html", logged_in=logged_in())
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
+
+
+@app.route("/favourites.html")
+def favourites():
+    if "user_id" not in session:
+        return redirect("/login.html")
+    else:
+        content = dbHandler.get_favourites(session["user_id"])
+
+        return render_template(
+            "favourites.html", logged_in=logged_in(), content=content
+        )
+
+
+@app.route("/favourite/<int:vtuber_id>")
+def favourite(vtuber_id):
+    if "user_id" not in session:
+        return redirect("/login.html")
+
+    dbHandler.add_favourite(session["user_id"], vtuber_id)
+
+    return redirect(request.referrer)
+
+
+@app.route("/unfavourite/<int:vtuber_id>")
+def unfavourite(vtuber_id):
+    if "user_id" not in session:
+        return redirect("/login.html")
+
+    dbHandler.remove_favourite(session["user_id"], vtuber_id)
+
+    return redirect(request.referrer)
 
 
 if __name__ == "__main__":
